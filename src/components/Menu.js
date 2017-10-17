38c498cd78e1fd8ed90cbaf1e6b56e81b600f0d1
Hugh Simpson <hughesimpson@gmail.com>
2017-10-17 22:07:06 +0100

    Add icons to header

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,9 +2,19 @@ import React from 'react'
 import StyledLink from '../components/StyledLink'
 import { Page, Row, Column } from 'hedron'
 import styled, { ThemeProvider } from 'styled-components'
+import GitHubIcon from 'react-icons/lib/fa/github'
+import LinkedIn from 'react-icons/lib/fa/linkedin'
 
 export default () => (
-  <Row justifyContent="flex-end;">
+  <Row justifyContent="space-between;">
+    <MenuList>
+      <ListItem>
+        <LinkedIn />
+      </ListItem>
+      <ListItem>
+        <GitHubIcon />
+      </ListItem>
+    </MenuList>
     <MenuList>
       <ListItem>
         <MenuLink to="/">Home</MenuLink>
@@ -22,13 +32,23 @@ export default () => (
 const MenuList = styled.ul`
   list-style: none;
   text-align: center;
-  padding: 20px 20px 30px 0;
+  margin: 2rem 0 0 0;
+  padding: 0;
+
+  svg {
+    height: 1.5rem;
+    width: 1.5rem;
+  }
 `
 
 const ListItem = styled.li`
   display: inline;
   font-family: ${props => props.theme.headingFont};
   padding-right: 1.5rem;
+
+  :first-child {
+    padding-left: 1rem;
+  }
 
   @media (max-width: 770px) {
     padding: 0.5rem;
