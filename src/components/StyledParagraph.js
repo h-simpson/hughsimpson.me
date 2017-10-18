c7e50a48750ad7b8f5c68056c900b62db9247ce1
h-simpson <hughesimpson@gmail.com>
2017-10-18 22:14:03 +0100

    Minor styling fix

--- src/components/StyledParagraph.js
+++ src/components/StyledParagraph.js
@@ -4,7 +4,6 @@ import styled from 'styled-components'
 export default styled.p`
   font-family: ${props => props.theme.bodyFont};
   font-size: 1.2rem;
-  padding: 0 1rem;
   margin: 0;
 
   @media (max-width: 900px) {
