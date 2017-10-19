4b63e126fdcd51372504788b8711d022827af704
h-simpson <hughesimpson@gmail.com>
2017-10-19 22:49:55 +0100

    Backgound color fix on mobile

--- src/styles/globalStyle.js
+++ src/styles/globalStyle.js
@@ -4,6 +4,10 @@ injectGlobal`
   body {
     margin: 0;
     box-sizing: border-box;
-    background-color: #f6f9fc;
+
+    //TODO Take out of injected global styles
+    @media (min-width: 770px) {
+      background-color: #f6f9fc;
+    }
   }
 `
