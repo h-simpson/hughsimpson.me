78570fb6ec7a6712fc4d0cb7b5f5e0f3f0f9d965
h-simpson <hughesimpson@gmail.com>
2017-10-25 20:19:20 +0100

    Minor UI improvements

--- src/components/BlogPostPreview.js
+++ src/components/BlogPostPreview.js
@@ -33,18 +33,19 @@ const PostCard = styled(Column)`
   @media (min-width: 770px) {
     padding: 3.5rem 3.15rem;
     background-color: #fff;
-    border-radius: 4px;
+    border-radius: 8px;
+    border-top: 5px solid #a897ec;
     box-shadow: rgba(25, 17, 34, 0.05) 0px 3px 10px;
     transition: transform 250ms cubic-bezier(0.4, 0, 0.2, 1), box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1);
 
     &:hover {
       box-shadow: rgba(25, 17, 34, 0.1) 0px 10px 42px;
-      transform: translateY(-4px);
+      transform: translateY(-7px);
     }
 
     &:active {
       box-shadow: rgba(25, 17, 34, 0.1) 0px 10px 42px;
-      transform: translateY(-4px);
+      transform: translateY(-7px);
     }
   }
 `
@@ -56,15 +57,19 @@ const PostLink = styled(Link)`
 
 const PostTitle = styled.h1`
   font-family: ${props => props.theme.headingFont};
+  color: ${props => props.theme.BodyFontColor};
   font-size: 1.5rem;
   font-weight: bold;
+  color: #34434b;
 `
 
 const PostExcerpt = styled.p`
   font-family: ${props => props.theme.headingFont};
+  color: ${props => props.theme.secondaryBodyFontColor};
   font-size: 1rem;
   margin: 0;
   padding-bottom: 0.5rem;
+  line-height: 1.5;
 `
 
 const BlogAuthor = styled.p`
