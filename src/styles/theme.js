6c4cd0f9e84e90ea2e85cd3be7e240493e9def40
h-simpson <hughesimpson@gmail.com>
2017-10-26 22:52:44 +0100

    Minor styling change

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -10,7 +10,7 @@ const theme = {
   // Colors
   paletteFontPrimary: '#494e58',
   paletteFontSecondary: '#7f8698',
-  palettePrimary: '#9063bb',
+  palettePrimary: '#72718c',
   paletteSecondary: '#f4f7f9',
   paletteTertiary: '#eaebef'
 }
