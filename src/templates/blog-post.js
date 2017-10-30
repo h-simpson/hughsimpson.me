161318419557e21ad204a96312b7d38ae118d1f6
h-simpson <hughesimpson@gmail.com>
2017-10-30 21:39:22 +0000

    Blog page styling improvements

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,7 +3,6 @@ import Helmet from 'react-helmet'
 import Link from 'gatsby-link'
 import styled from 'styled-components'
 import get from 'lodash/get'
-import { Page, Row, Column } from 'hedron'
 
 class BlogPostTemplate extends React.Component {
   render() {
@@ -12,58 +11,68 @@ class BlogPostTemplate extends React.Component {
     const siteURL = get(this.props, 'data.site.siteMetadata.url')
 
     return (
-      <article>
-        <Row justifyContent="center">
-          <Helmet>
-            <title>{`${post.frontmatter.title} | ${siteTitle}`}</title>
-            <meta name="description" content={`${post.frontmatter.excerpt}`} />
-            <meta property="og:type" content="website" />
-            <meta property="og:url" content={`${siteURL}${post.frontmatter.path}`} />
-            <meta property="og:title" content={siteTitle} />
-            <meta property="og:description" content={`${post.frontmatter.excerpt}`} />
-            <meta property="og:site_name" content={siteTitle} />
-            <meta property="og:locale" content="en_UK" />
-          </Helmet>
-          <StyledColumn md={10} lg={8}>
-            <BlogTitle>{post.frontmatter.title}</BlogTitle>
-          </StyledColumn>
-        </Row>
-        <Row alignItems="baseline;" justifyContent="center">
-          <StyledColumn xs={6} sm={6} md={5} lg={6}>
-            <BlogDate>{post.frontmatter.date}</BlogDate>
-          </StyledColumn>
-          <StyledColumn xs={6} sm={6} md={5} lg={2}>
-            <BlogCategory>{post.frontmatter.category}</BlogCategory>
-          </StyledColumn>
-        </Row>
-        <Row justifyContent="center">
-          <StyledColumn md={10} lg={8}>
-            <BlogBody dangerouslySetInnerHTML={{ __html: post.html }} />
-          </StyledColumn>
-        </Row>
-      </article>
+      <BlogArticle>
+        <Helmet>
+          <title>{`${post.frontmatter.title} | ${siteTitle}`}</title>
+          <meta name="description" content={`${post.frontmatter.excerpt}`} />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content={`${siteURL}${post.frontmatter.path}`} />
+          <meta property="og:title" content={siteTitle} />
+          <meta property="og:description" content={`${post.frontmatter.excerpt}`} />
+          <meta property="og:site_name" content={siteTitle} />
+          <meta property="og:locale" content="en_UK" />
+        </Helmet>
+        <BlogTitle>{post.frontmatter.title}</BlogTitle>
+        <BlogDate>{post.frontmatter.date}</BlogDate>
+        {post.frontmatter.category && <BlogCategory>{post.frontmatter.category}</BlogCategory>}
+        <BlogBody dangerouslySetInnerHTML={{ __html: post.html }} />
+      </BlogArticle>
     )
   }
 }
 
 export default BlogPostTemplate
 
+const BlogArticle = styled.article`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+`
+
 const BlogTitle = styled.h1`
+  color: ${props => props.theme.paletteFontPrimary};
   font-family: ${props => props.theme.fontFamilyPrimary};
   font-size: 2rem;
   text-decoration: none;
+  width: 70%;
+  max-width: 800px;
+  padding: 0;
+  margin-top: 2rem;
 `
 const BlogCategory = styled.h4`
+  color: ${props => props.theme.paletteFontPrimary};
   font-family: ${props => props.theme.fontFamilyPrimary};
   font-size: 1.5rem;
+  max-width: 800px;
 `
 const BlogDate = styled.h3`
+  color: ${props => props.theme.paletteFontPrimary};
   font-family: ${props => props.theme.fontFamilyPrimary};
   font-size: 1.3rem;
+  width: 70%;
+  max-width: 800px;
+  padding: 0;
+  margin-top: 1rem;
 `
-
 const BlogBody = styled.section`
+  color: ${props => props.theme.paletteFontPrimary};
   font-family: ${props => props.theme.fontFamilySecondary};
+  width: 70%;
+  max-width: 800px;
+  padding: 0;
+  margin-top: 2rem;
   h1,
   h2,
   h3,
@@ -73,10 +82,16 @@ const BlogBody = styled.section`
     font-family: ${props => props.theme.fontFamilyPrimary};
   }
   font-size: 1.3rem;
-`
 
-const StyledColumn = styled(Column)`
-  padding-top: 0;
+  :before {
+    width: 30px;
+    height: 6px;
+    background-color: ${props => props.theme.paletteTertiary};
+    display: block;
+    content: '';
+    position: absolute;
+    margin-top: -1rem;
+  }
 `
 
 export const pageQuery = graphql`
