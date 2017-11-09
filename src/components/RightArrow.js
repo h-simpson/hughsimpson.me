c4a26765497a28785bfc6a7433632312508273a3
h-simpson <hughesimpson@gmail.com>
2017-11-09 20:30:12 +0000

    SVG bug fix

--- src/components/RightArrow.js
+++ src/components/RightArrow.js
@@ -3,15 +3,8 @@ import styled from 'styled-components'
 
 export default () => (
   <SVG width="15px" height="20px" viewBox="0 0 50 80">
-    <polyline
-      fill="none"
-      stroke="#7f8698"
-      stroke-width="30"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      points="
-	0.375,0.375 45.63,38.087 0.375,75.8 "
-    />
+    <polyline fill="none" stroke="#7f8698" points="
+	0.375,0.375 45.63,38.087 0.375,75.8 " />
   </SVG>
 )
 
