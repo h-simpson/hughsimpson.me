c2d32b7d4739755a841aab9b053577e61171c8f6
h-simpson <hughesimpson@gmail.com>
2017-11-20 20:18:07 +0000

    Minor styling improvement

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,20 +1,29 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import BlogPostPreview from '../components/BlogPostPreview'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  width: 100%;
+  padding: 0 20px;
+  max-width: 690px;
+`
 
 export default ({ data }) => {
   const siteTitle = data.site.siteMetadata.title
   const posts = data.allMarkdownRemark.edges
   return (
-    <section>
-      {<Helmet title={siteTitle} />}
-      {posts.map(post => {
-        if (post.node.path !== '/404/') {
-          const title = post.node.frontmatter.title || post.node.path
-          return <BlogPostPreview {...post.node.frontmatter} key={title} />
-        }
-      })}
-    </section>
+    <main>
+      <Container>
+        {<Helmet title={siteTitle} />}
+        {posts.map(post => {
+          if (post.node.path !== '/404/') {
+            const title = post.node.frontmatter.title || post.node.path
+            return <BlogPostPreview {...post.node.frontmatter} key={title} />
+          }
+        })}
+      </Container>
+    </main>
   )
 }
 
