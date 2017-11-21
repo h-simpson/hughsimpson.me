a4c5b9cc68ebecc5fa21c9109df6558207d61b75
h-simpson <hughesimpson@gmail.com>
2017-11-21 22:46:07 +0000

    Modal styling

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -19,19 +19,23 @@ const ModalContainer = styled.div`
   left: 0;
   right: 0;
   bottom: 0;
-  background: rgba(0, 0, 0, 0.3);
+  background: rgba(0, 0, 0, 0.5);
   z-index: 999;
   padding: 50px;
 `
 
 const ModalContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   width: 100%;
-  max-width: 800px;
+  max-width: 600px;
   min-height: 200px;
   border-radius: 4px;
   background-color: ${props => props.theme.paletteTertiary};
   box-shadow: rgba(25, 17, 34, 0.05) 0px 3px 10px;
-  padding: 3rem;
+  padding: 5rem 2rem;
 `
 
 export default Modal
